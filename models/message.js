0fe13a1a2c1459c46b243025ad85a85039d3fd79
Martinez Alban <martinez.alban25@gmail.com>
2017-01-09 23:59:31 +0100

    09/01/2017 Alban / Routes MAJ chat

--- models/message.js
+++ models/message.js
@@ -27,7 +27,7 @@ function message()
     {
         connection.acquire(function(err, con) 
         {
-            con.query('insert into message(message, dateMessage, idUser, idUser_Users, read) VALUES (?, now(), ?, ?, ?)', [message.message, message.idUser, message.idUser_Users, message.read], function(err, result) 
+            con.query('insert into message(message, dateMessage, idUser, idUser_Users, readStatus) VALUES (?, now(), ?, ?, ?)', [message.message, message.idUser, message.idUser_Users, message.readStatus], function(err, result) 
             {
                 con.release();
                 if (err) 
@@ -51,13 +51,38 @@ function message()
     {
         connection.acquire(function(err, con) 
         {
-            con.query('select u.lastname, u.firstname, u2.lastname, u2.firstname, message, dateMessage from Message'+
+            con.query('select * from Message'+
             'INNER JOIN Users u ON u.idUser = Message.idUser'+
             'INNER JOIN Users u2 ON u2.idUser = Message.idUser_Users'+
             'WHERE Message.idUser = ? AND Message.idUser_Users = ?', [iduser, iduser2],  function(err, result) 
             {
                 con.release();
                 res.send(result);
+            });
+        });
+    };
+
+    /**
+     * Delete a specific message
+     * @params id message's id
+     * @params res response
+     */
+    this.markAsRead = function(id, res)
+    {
+        connection.acquire(function(err, con)
+        {
+            con.query('UPDATE Message SET readStatus = true where idMessage = ?', [id], function(err, result) 
+            {
+                con.release();
+                if (err) 
+                {   
+                    console.log(err);
+                    res.send({status: 1, message: 'Failed to update readStatus'});
+                } 
+                else 
+                {
+                    res.send({status: 0, message: 'successfully updated readStatus'});
+                }
             });
         });
     };
